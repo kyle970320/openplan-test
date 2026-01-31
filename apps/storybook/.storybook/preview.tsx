db42fb3d3f7dbd67adeca53f1d8db9fa69b3dcd1
kyle <park970320@gmail.com>
2026-02-01 01:11:09 +0900

    feat(storybook): add Storybook with Button stories

---
+++ apps/storybook/.storybook/preview.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import type { Preview } from "@storybook/react";
+import "@openplan-test/ui/style.css";
+import "../src/index.css";
+
+const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <div
+        className="w-full min-h-screen flex justify-center items-center"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100vh",
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@openplan-test/ui/style.css";
 import App from "./App";
 import "./index.css";
 
