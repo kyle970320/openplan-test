c9f898afba86eb6b72b3d2f4abb65f9ad9dc7dda
kyle <park970320@gmail.com>
2026-02-02 00:04:50 +0900

    feat: add bg and detail data in result page

--- apps/web/src/pages/Result.tsx
+++ apps/web/src/pages/Result.tsx
@@ -1,24 +1,88 @@
+import { Button } from "@openplan-test/ui";
 import { usePhotoStore } from "../app/store/photoStore";
+import { useNavigate } from "react-router-dom";
 
 export default function ResultPage() {
   const photoInfo = usePhotoStore((s) => s.photoInfo);
-
+  const navigate = useNavigate();
   if (!photoInfo) {
     return (
-      <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center gap-4 p-8">
+      <div className="h-full flex flex-col items-center justify-center gap-4">
         <p className="text-gray-600">조회된 사진 정보가 없습니다.</p>
       </div>
     );
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col items-center gap-6 p-8">
-      <h1 className="text-2xl font-bold text-gray-900">사진 조회 결과</h1>
-      <img
-        src={photoInfo.download_url}
-        alt={photoInfo.author}
-        className="w-full rounded object-cover"
-      />
+    <div className="relative h-full flex flex-col items-center gap-6">
+      <div className="fixed top-0 left-0 w-full h-full scale-115">
+        <img
+          src={photoInfo.download_url}
+          alt={"info-image"}
+          className="absolute top-0 left-0 w-full h-full object-cover opacity-40 blur-[6px]"
+        />
+        <div className="absolute top-0 left-0 w-full h-full bg-linear-to-b from-transparent to-[#ffffff]"></div>
+        <div className="absolute top-0 left-0 w-full h-full bg-black/30" />
+        <img
+          src="/noiseLayer.png"
+          alt="noise-layer"
+          className="absolute top-0 left-0 w-full h-full rounded object-cover"
+        />
+      </div>
+      <div className="flex-1 relative w-full flex gap-10 items-center justify-center">
+        <img
+          src={photoInfo.download_url}
+          alt={photoInfo.author}
+          className="flex-1 w-1/2 rounded-3xl"
+        />
+        <div className="flex-1 flex flex-col items-center gap-3">
+          <div className="flex w-full p-5 rounded-2xl bg-white">
+            <div className="flex-1">
+              <p>id</p>
+              <p className="text-text-primary opacity-50">{photoInfo.id}</p>
+            </div>
+            <div className="flex-1">
+              <p>author</p>
+              <p className="text-text-primary opacity-50">{photoInfo.author}</p>
+            </div>
+          </div>
+          <div className="flex w-full p-5 rounded-2xl bg-white">
+            <div className="flex-1">
+              <p>width</p>
+              <p className="text-text-primary opacity-50">{photoInfo.width}</p>
+            </div>
+            <div className="flex-1">
+              <p>height</p>
+              <p className="text-text-primary opacity-50">{photoInfo.height}</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 w-full p-5 rounded-2xl bg-white">
+            <div className="flex-1">
+              <p>url</p>
+              <a
+                href={photoInfo.url}
+                target="_blank"
+                className="text-text-primary opacity-50 underline"
+              >
+                {photoInfo.url}
+              </a>
+            </div>
+            <div className="flex-1">
+              <p>download_url</p>
+              <a
+                href={photoInfo.download_url}
+                target="_blank"
+                className="text-text-primary opacity-50 underline"
+              >
+                {photoInfo.download_url}
+              </a>
+            </div>
+          </div>
+          <Button variant="secondary" size="sm" onClick={() => navigate("/")}>
+            <span>이전</span>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
