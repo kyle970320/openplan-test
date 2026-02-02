dc04c6750e6b523db56ded4dd031bda77ab0fe5d
minkyu_Park <kyle970320@farmzonesnc.com>
2026-02-02 13:35:52 +0900

    feat: modify throttle with loading state

--- apps/web/src/shared/hooks/useThrottle.ts
+++ apps/web/src/shared/hooks/useThrottle.ts
@@ -1,4 +1,4 @@
-import { useCallback, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 /**
  * Throttle + signle process
@@ -8,6 +8,10 @@ import { useCallback, useRef } from "react";
  * 정해진 시간을 안에는 호출되지 않음
  *
  * 이미 실행중인 프로세스가 있다면 해당 프로세스를 반환
+ *
+ * isBlocked: 정해진 시각 안쪽에서는 항상 loading 상태
+ *
+ * isProcessing: 현재 프로세스가 실행중이면 항상 loading 상태
  */
 export function useThrottle<Args extends unknown[], Result>(
   func: (...args: Args) => Promise<Result>,
@@ -16,21 +20,48 @@ export function useThrottle<Args extends unknown[], Result>(
   const lastCalledRef = useRef<number>(0);
   const processingRef = useRef<Promise<Result> | null>(null);
 
-  return useCallback(
+  const unblockTimerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  const [isProcessing, setIsProcessing] = useState<boolean>(false);
+  const [isBlocked, setIsBlocked] = useState<boolean>(false);
+  const isThrottledLoading = isProcessing || isBlocked;
+
+  useEffect(() => {
+    return () => {
+      if (unblockTimerRef.current) clearTimeout(unblockTimerRef.current);
+    };
+  }, []);
+
+  const throttledFunc = useCallback<(...args: Args) => Promise<Result> | undefined>(
     (...args: Args): Promise<Result> | undefined => {
       if (processingRef.current) {
         return processingRef.current;
       }
 
       const now = Date.now();
-      if (now - lastCalledRef.current < wait) {
+      const remain = wait - (now - lastCalledRef.current);
+      if (remain > 0) {
+        setIsBlocked(true);
+
+        if (unblockTimerRef.current) {
+          clearTimeout(unblockTimerRef.current);
+        }
+
+        unblockTimerRef.current = setTimeout(() => {
+          setIsBlocked(false);
+          unblockTimerRef.current = null;
+        }, remain);
+
         return;
       }
 
       lastCalledRef.current = now;
+      setIsBlocked(false);
+      setIsProcessing(true);
 
       const processing = func(...args).finally(() => {
         processingRef.current = null;
+        setIsProcessing(false);
       });
 
       processingRef.current = processing;
@@ -38,4 +69,6 @@ export function useThrottle<Args extends unknown[], Result>(
     },
     [func, wait]
   );
+
+  return { throttledFunc, isThrottledLoading };
 }
